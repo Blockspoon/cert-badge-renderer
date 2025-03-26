d01e095d65ad5dce89384549c612131db3bc0e1e
Jiyong Kim <161813429+CareerSupporter@users.noreply.github.com>
2025-03-26 12:33:27 +0900

    [UPDATE] build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,3 @@
-export { renderCertificate } from "./renderCertificate";
+export { renderCertificate } from './renderCertificate';
+export { generateCertificateFile } from './utils/generateCertificateFile';
+export type { CertificateData, CertificateOptions } from './interface';
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.renderCertificate = void 0;
+exports.generateCertificateFile = exports.renderCertificate = void 0;
 var renderCertificate_1 = require("./renderCertificate");
 Object.defineProperty(exports, "renderCertificate", { enumerable: true, get: function () { return renderCertificate_1.renderCertificate; } });
+var generateCertificateFile_1 = require("./utils/generateCertificateFile");
+Object.defineProperty(exports, "generateCertificateFile", { enumerable: true, get: function () { return generateCertificateFile_1.generateCertificateFile; } });
 // export { getBindingValue } from "./utils/getBindingValue";
