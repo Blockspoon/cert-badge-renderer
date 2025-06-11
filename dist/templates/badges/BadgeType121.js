10d7d8b5d83fed88e2ff0913d7aa3935c45abbdb
Jiyong Kim <161813429+CareerSupporter@users.noreply.github.com>
2025-06-11 19:33:08 +0900

    [FIX] design errors

--- dist/templates/badges/BadgeType121.js
+++ dist/templates/badges/BadgeType121.js
@@ -7,7 +7,7 @@ function createBadgeType121({ mainColor = "#96A6B4", subColor = "#96A6B4", extra
       width="100%"
       height="100%"
       viewBox="0 0 536 526"
-      fill="none"
+      fill="white"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
--- src/templates/badges/BadgeType121.ts
+++ src/templates/badges/BadgeType121.ts
@@ -11,7 +11,7 @@ export function createBadgeType121({
       width="100%"
       height="100%"
       viewBox="0 0 536 526"
-      fill="none"
+      fill="white"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
